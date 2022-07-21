ce137a27ba655e3a07189299b2f140326eb038c7
Phạm Trung Kỳ <44194038+ptky18it4@users.noreply.github.com>
2022-07-21 22:29:52 +0700

    Done creating a Task (Part 2 Controller)

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { title } from 'process';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -10,5 +11,13 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    return this.tasksService.createTask(title, description);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
-import * as uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
@@ -16,7 +16,7 @@ export class TasksService {
 
   createTask(title: string, description: string): Task {
     const task: Task = {
-      id: uuid(),
+      id: uuidv4(),
       title: title,
       description: description,
       status: TaskStatus.OPEN,
