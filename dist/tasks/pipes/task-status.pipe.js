5193b639b741e349e336fcd5099c7a4a19c63103
Phạm Trung Kỳ <44194038+ptky18it4@users.noreply.github.com>
2022-07-24 16:11:14 +0700

    Coding: Custom Pipe Validating the Task Status

---
+++ dist/tasks/pipes/task-status.pipe.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=task-status.pipe.js.map
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -15,6 +15,7 @@ import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
@@ -44,7 +45,7 @@ export class TasksController {
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
   ): Task {
     return this.tasksService.updateTaskStatus(id, status);
   }
