8e52ccec4b774cdda8e24faef9253632e7383067
Phạm Trung Kỳ <44194038+ptky18it4@users.noreply.github.com>
2022-07-24 16:38:28 +0700

    Coding: ValidationPipe Task Filtering and Search

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,7 +1,12 @@
 /* eslint-disable prettier/prettier */
+import { IsIn, IsNotEmpty, IsOptional } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
   status: TaskStatus;
+  @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
